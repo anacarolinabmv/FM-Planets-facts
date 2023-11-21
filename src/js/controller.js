2c0a35a9316512ef91a01e99ecb532a395e1c3d8
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-11-21 18:37:53 +0100

    Implement mobile navigation and design

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@ import * as model from './model.js';
 import navigationView from './views/navigationView.js';
 import planetView from './views/planetView.js';
 
-const controlNavButtons = function (query) {
+const controlNavLinks = function (query) {
   model.state.query = query;
   planetView.render(model.state);
 };
@@ -12,6 +12,9 @@ const controlViewButtons = function (view) {
   planetView.render(model.state, view);
 };
 
+const bleh = function () {
+  console.log('bleh');
+};
 const init = async function () {
   //1) Get data
   await model.getData();
@@ -19,11 +22,14 @@ const init = async function () {
   //2) Render navigation buttons
   navigationView.render(model.state.planets);
 
+  //3) Add handler to the nav button
+  navigationView.addHandlerNavBtn(bleh);
+
   //3) Render app load view (Mercury)
   planetView.render(model.state);
 
   //3) Add the handlers to the views
-  navigationView.addHandler(controlNavButtons);
+  navigationView.addHandlerNavLinks(controlNavLinks);
   planetView.addHandler(controlViewButtons);
 };
 
--- src/js/views/navigationView.js
+++ src/js/views/navigationView.js
@@ -1,13 +1,23 @@
 class NavigationView {
+  navBtn = document.querySelector('.navigation__btn');
   #parentElement = document.querySelector('.navigation__planets-list');
   #data;
 
-  addHandler(handler) {
+  addHandlerNavLinks(handler) {
     this.#parentElement.addEventListener('click', function (e) {
+      console.log(e);
       if (!e.target.classList.contains('navigation__link')) return;
       const query = e.target.textContent;
 
       handler(query);
+    });
+  }
+
+  addHandlerNavBtn() {
+    this.navBtn.addEventListener('click', function () {
+      const navigationBar = this.closest('.navigation').querySelector('.navigation__planets-list');
+
+      navigationBar.classList.toggle('show');
     });
   }
 
@@ -20,7 +30,8 @@ class NavigationView {
   #generateMarkup() {
     return this.#data.reduce((markup, cur) => {
       markup += `<li class="navigation__list-item">
-          <a href="#" class="navigation__link h4 navigation__link--${cur.name.toLowerCase()}">${cur.name}</a>
+                <a href="#" class="navigation__link h4 navigation__link--${cur.name.toLowerCase()}">${cur.name}</a>
+          <span class="icon--arrow"><i class="fa-solid fa-chevron-right"></i></span>
           </li>`;
 
       return markup;
